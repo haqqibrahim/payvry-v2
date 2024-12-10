1e926da5439bf534ef7673860b90d623f00316be
haqqibrahim <enessyibrahim@gmail.com>
2024-12-10 06:58:14 +0100

    port

--- Backend/app.py
+++ Backend/app.py
@@ -82,6 +82,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=5000,
+        port=int(os.getenv("PORT", 5000)),
         reload=True
     )
